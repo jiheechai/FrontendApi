687d15114dd054ca3e15e871994aa770cd686f8e
jiheechai <jjihee97@naver.com>
2024-09-29 17:33:07 +0900

    login donegit add .!

--- daydream/src/features/LoginPage/index.tsx
+++ daydream/src/features/LoginPage/index.tsx
@@ -1,5 +1,6 @@
 import { LoginPageStyled } from "@/features/LoginPage/styled";
 import { KAKAO_AUTH_URL } from "./KakaoAuthUrl";
+import KakaoLoginButton from "@/components/KakaoLoginButton";
 const LoginPage = () => {
   const handleLogin = () => {
     if (typeof window !== "undefined") {
@@ -12,12 +13,13 @@ const LoginPage = () => {
     <LoginPageStyled>
       <h1>로그인 화면입니다.</h1>
       {/* <KakaoButton imgUrl={logoBtnImg} onClick={handleLogin} /> */}
-      <button onClick={handleLogin}>
+      {/* <button onClick={handleLogin}>
         <img
           src="https://developers.kakao.com/assets/img/about/logos/kakaologin/kr/kakao_account_login_btn_medium_narrow.png"
           alt="카카오 로그인"
         />
-      </button>
+      </button> */}
+      <KakaoLoginButton />
     </LoginPageStyled>
   );
 };
--- daydream/src/features/auth/AuthCallback/index.tsx
+++ daydream/src/features/auth/AuthCallback/index.tsx
@@ -1,34 +1,99 @@
 import { AuthCallbackStyledComponent } from "./styled";
-import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import axios from "axios";
+interface UserData {
+  nickname: string;
+  profile_image_url: string;
+}
 
 const AuthCallback = () => {
-  // const navigate = useNavigate();
+  const router = useRouter();
+  const [userData, setUserData] = useState<UserData | null>(null);
 
   useEffect(() => {
-    const code = new URL(window.location.href).searchParams.get("code");
-    console.log(code);
-    const headers = {
-      "Content-Type": "application/x-www-form-urlencoded",
+    const fetchKakaoToken = async () => {
+      const { code } = router.query;
+
+      if (code) {
+        try {
+          const response = await axios.post(
+            "http://localhost:5000/api/auth/kakao/callback",
+            {
+              code: code,
+            }
+          );
+
+          console.log(response.data.user);
+
+          // 카카오 로그인 성공 후, 사용자 정보 저장
+          const { nickname, profile_image_url } =
+            response.data.user.kakao_account.profile;
+
+          setUserData({
+            nickname: nickname,
+            profile_image_url: profile_image_url,
+          });
+
+          console.log("로그인 성공:", response.data);
+        } catch (error) {
+          console.error("카카오 로그인 실패:", error);
+        }
+      }
     };
 
-    // fetch(`보내줄 주소?code=${code}`, {
-    //   method: "POST",
-    //   headers: headers,
-    // })
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     console.log(data);
-    //     console.log(data.result.user_id);
-    //     console.log(data.result.jwt);
-    //   })
-    //   .catch((error) => {
-    //     console.error("오류 발생", error);
-    //   });
-  }, []);
+    fetchKakaoToken();
+  }, [router.query]);
+
   return (
     <AuthCallbackStyledComponent>
-      <h1>카카오 로그인이 완료되면 보이는 화면입니다.</h1>
+      <div>
+        {userData ? (
+          <div>
+            <h1>Welcome, {userData.nickname}!</h1>
+            <img src={userData.profile_image_url} alt="Profile" />
+          </div>
+        ) : (
+          <p>로그인 중...</p>
+        )}
+      </div>
     </AuthCallbackStyledComponent>
   );
 };
+
 export default AuthCallback;
+
+// import { AuthCallbackStyledComponent } from "./styled";
+// import { useEffect } from "react";
+
+// const AuthCallback = () => {
+//   // const navigate = useNavigate();
+
+//   useEffect(() => {
+//     const code = new URL(window.location.href).searchParams.get("code");
+//     console.log(code);
+//     const headers = {
+//       "Content-Type": "application/x-www-form-urlencoded",
+//     };
+
+//     // fetch(`보내줄 주소?code=${code}`, {
+//     //   method: "POST",
+//     //   headers: headers,
+//     // })
+//     //   .then((response) => response.json())
+//     //   .then((data) => {
+//     //     console.log(data);
+//     //     console.log(data.result.user_id);
+//     //     console.log(data.result.jwt);
+//     //   })
+//     //   .catch((error) => {
+//     //     console.error("오류 발생", error);
+//     //   });
+//   }, []);
+//   return (
+//     <AuthCallbackStyledComponent>
+//       <h1>카카오 로그인이 완료되면 보이는 화면입니다.</h1>
+//     </AuthCallbackStyledComponent>
+//   );
+// };
+// export default AuthCallback;
